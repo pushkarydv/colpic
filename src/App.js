90d77d5b49ea9e7d76a9a28bfaac4bb71a4f5d68
pushkarydv <96358784+pushkarydv@users.noreply.github.com>
2022-02-03 11:03:54 +0530

    d - removed

--- src/App.js
+++ src/App.js
@@ -9,10 +9,6 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Gradients from "./pages/Gradients";
-import Download from "./pages/Download";
-import C, { FetchColor } from "./pages/C";
-import G from "./pages/G";
-
 export default function App() {
   return (
     <Router>
@@ -21,11 +17,6 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/gradients" element={<Gradients />} />
-        <Route path="/download" element={<Download />} />
-        <Route path="/download/c" element={<C />}>
-          <Route path=":colorName" element={<FetchColor />} />
-        </Route>
-        <Route path="/download/g" element={<G />} />
         <Route path="/colors" element={<Navigate replace to="/" />} />
         <Route path="/home" element={<Navigate replace to="/" />} />
       </Routes>
--- src/pages/About.js
+++ src/pages/About.js
@@ -8,17 +8,19 @@ export default function About() {
       <Nav />
       <div className="f500 f1-5 m1">About COLPIC</div>
       <p className="m1">
-        This is basically a free color picker, you just have to click on colors
-        or gradients to copy them and paste them into your projects directly.{" "}
-        <br />
+        This is basically a free color and gradient picker, you just have to
+        click on colors or gradients to copy them and paste them into your
+        projects directly. <br />
         This colorpicker is dedicated to make use on these amazing color codes
         easy, we remember them so that you don't have to remember them everyday.{" "}
         <br />
         Build Some Great Projects Now😎
       </p>
-      <p className="m1">Built in React Js</p>
 
-      <p className="f500 m1">Try out my other tools</p>
+      <p className="f500 m1">
+        This project is open source check my Github for this and more such cool
+        projects
+      </p>
       <p className="m1">
         <GoA
           name="https://github.com/pushkarydv"
